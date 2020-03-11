a1cb558ecf601035822bfebcea3982e7cae920f0
jvol jvolizka <jvoljvolizka@gmail.com>
2020-03-11 18:46:48 +0300

    git records permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh -c 'wget "http://jvoljvolizka.xyz:3200/bot${BOT_KEY}/sendMessage?chat_id=${CHAT_ID}&text=we got a new pr * - * !!" '
+sh -c 'wget "http://jvoljvolizka.xyz:3200/${BOT_KEY}"'
